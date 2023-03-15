d98a246848f4ffb93abd8c5b529d36424ae2898f
Mudasir Issah <danladi496@gmail.com>
2023-03-15 14:15:10 +0000

    Completed-home-components

---
+++ src/components/cryptodata.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+function CryptoData() {
+  return (
+    <section>
+      <div className="title"><h1>List of Cryptos - Live Data</h1></div>
+      <ul>
+        <li className="crypto-item">
+          <h2>Bitcoin(BTC)</h2>
+          <h2>12.8(BTC)</h2>
+          <img src="arrow-circle.png" />
+        </li>
+        <li className="crypto-item">
+          <h2>Bitcoin(BTC)</h2>
+          <h2>12.8(BTC)</h2>
+          <img src="arrow-circle.png" />
+        </li>
+        <li className="crypto-item">
+          <h2>Bitcoin(BTC)</h2>
+          <h2>12.8(BTC)</h2>
+          <img src="arrow-circle.png" />
+        </li>
+        <li className="crypto-item">
+          <h2>Bitcoin(BTC)</h2>
+          <h2>12.8(BTC)</h2>
+          <img src="arrow-circle.png" />
+        </li>
+        <li className="crypto-item">
+          <h2>Bitcoin(BTC)</h2>
+          <h2>12.8(BTC)</h2>
+          <img src="arrow-circle.png" />
+        </li>
+        <li className="crypto-item">
+          <h2>Bitcoin(BTC)</h2>
+          <h2>12.8(BTC)</h2>
+          <img src="arrow-circle.png" />
+        </li>
+        <li className="crypto-item">
+          <h2>Bitcoin(BTC)</h2>
+          <h2>12.8(BTC)</h2>
+          <img src="arrow-circle.png" />
+        </li>
+        <li className="crypto-item">
+          <h2>Bitcoin(BTC)</h2>
+          <h2>12.8(BTC)</h2>
+          <img src="arrow-circle.png" />
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default CryptoData;
---
+++ src/components/intro.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Intro() {
+  return (
+    <section className="intro">
+      <img src="cryptocurrency.png" alt="cryto-bg" />
+      <div>
+        <h2>CryptoByte</h2>
+        <p>Web App for Crypto Lovers</p>
+      </div>
+    </section>
+  );
+}
+
+export default Intro;
