a61f9557edfda9bfc435cd523c73bbddc0d53f9a
Mudasir Issah <danladi496@gmail.com>
2023-03-16 02:39:41 +0000

    Fixed-linters

--- src/tests/__snapshots__/app.test.js
+++ src/tests/__snapshots__/app.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import { MemoryRouter as Router } from 'react-router-dom';
-import store from '../redux/store';
+import store from '../../redux/store';
 import App from '../../App';
 
 describe('Testing App component', () => {
